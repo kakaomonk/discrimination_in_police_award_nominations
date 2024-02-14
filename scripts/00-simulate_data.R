2a04684eaf40528ec39f41eb437ff7e6848c9659
Hyungsoo Park <hyungsoo.park@mail.utoronto.ca>
2024-02-14 20:34:08 +0000

    Updated simulation / Added final lm table.

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,26 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulate each race/gender's award score
+# Author: Hyungsoo Park, Jeongwoo Kim
+# Date: 12 February 2024
+# Contact: hyungsoo.park@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: R
+# Any other information needed? NA
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+# set seed for the simulation
+set.seed(1002415488)
+number_simulations <- 10000
+# set a distribution of award score with normal distribution (mean: 7.054227, sd: 4.553837 based on the raw-data).
+races <- c("White", "Asian", "Black", "Natam", "Hisp")
+# Simulate 10000 times
+sim_race <- replicate(number_simulations, c(sample(races, 1, replace = TRUE), rtruncnorm(1, a = 0, mean = mean(all_officer$awd_perf), sd = sd(all_officer$awd_perf))))
 
+gender <- c("Male", "Female")
+sim_gender <- replicate(number_simulations, c(sample(gender, 1, replace = TRUE), rtruncnorm(1, a = 0, mean = mean(all_officer$awd_perf), sd = sd(all_officer$awd_perf))))
 
 
